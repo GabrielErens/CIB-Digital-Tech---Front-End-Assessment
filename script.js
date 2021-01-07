315876756994120f821e5966b6d6543c3bdc8ba9
Erens Oosthuizen <erensoosthuizen3@gmail.com>
2021-01-07 19:51:36 +0200

    Second Commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+function bgChager(){
+
+    //Select a Random number between 1-5
+    var randomImg = Math.floor(Math.random() * 5 + 1);
+
+
+    //selects an random img from the img folder 
+    var bgImg = "Image/Image" + randomImg + ".jpg"
+
+      // assigns an random background image to first img
+  document.querySelectorAll("img")[0].setAttribute("src", bgImg);
+
+  return bgImg;
+}
+
+window.onload = document.body.style.backgroundImage = "url('bgChanger')";
+
