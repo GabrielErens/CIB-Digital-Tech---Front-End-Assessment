3ea8134b1a9be2c5dbb1557b97297c87e468d977
Erens Oosthuizen <erensoosthuizen3@gmail.com>
2021-01-08 12:18:22 +0200

    Final Commit

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function bgChager(){
 
-    //Select a Random number between 1-5
-    var randomImg = Math.floor(Math.random() * 5 + 1);
+//Show wat button is active or clicked on
+var divName = document.getElementsByClassName("btnForm");
+var btns = document.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+};
 
+//Show wat page is selected in Second Navigation bar
+var divNameLinks = document.getElementsByClassName("links");
+var btnAnkerSecondNavJs = document.getElementsByClassName("ankerSecondNavJs");
+for (var i = 0; i < btnAnkerSecondNavJs.length; i++) {
+  btnAnkerSecondNavJs[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("activeSecondNav");
+  current[0].className = current[0].className.replace(" activeSecondNav", "");
+  this.className += " activeSecondNav";
+  });
+};
 
-    //selects an random img from the img folder 
-    var bgImg = "Image/Image" + randomImg + ".jpg"
-
-      // assigns an random background image to first img
-  document.querySelectorAll("img")[0].setAttribute("src", bgImg);
-
-  return bgImg;
-}
-
-window.onload = document.body.style.backgroundImage = "url('bgChanger')";
-
+//Show wat button is active on Form Links
+var divNameFormLinks = document.getElementsByClassName("forumStyleAnkerTags");
+var formlink = document.getElementsByClassName("formAnkerTags");
+for (var i = 0; i < formlink.length; i++) {
+  formlink[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("activeFormLink");
+  current[0].className = current[0].className.replace(" activeFormLink", "");
+  this.className += " activeFormLink";
+  });
+};
